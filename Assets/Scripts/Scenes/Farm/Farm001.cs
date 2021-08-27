f6839efbc848129813ca33527bb4a5faeedb23ca
Jonathan Dyason <jonathandyason@hotmail.com>
2021-08-27 16:19:19 +0200

    sdf

---
+++ Assets/Scripts/Scenes/Farm/Farm001.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace V4
+{
+    public class Farm001 : MonoBehaviour
+    {
+        void Start()
+        {
+
+        }
+        public void AnswerButton01()
+        {
+            SceneManager.LoadScene("Farm002");
+        }
+
+    }
+}
---
+++ Assets/Scripts/Scenes/Town/TownIntro.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace V4
+{
+    public class TownIntro : MonoBehaviour
+    {
+        private float timeRemaining = 2;
+
+        void Update()
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+            }
+            else
+            {
+                SceneManager.LoadScene("Town001");
+            }
+        }
+    }
+}
